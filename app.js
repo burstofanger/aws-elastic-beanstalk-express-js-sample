9902b28f7cd63bd4c17af07909dd1bfdd9c66cfd
Yen <lee-kyen@hotmail.com>
2021-01-13 22:40:39 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('change job pls'));
+app.get('/', (req, res) => res.send('change job pls oh pretty pls'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
